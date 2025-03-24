a8d3aa10f52909c6396a11e1fd5168427fb280d2
Charlize1884 <cbololoi@students.uni-mainz.de>
2025-03-24 10:22:19 +0100

    adding exit

--- EiP-Praktikum/world.py
+++ EiP-Praktikum/world.py
@@ -9,6 +9,7 @@ class World():
         jump_pad_img = pygame.image.load("assets/Jumping_Pad_4.png")
         jump_pad_img2 = pygame.image.load("assets/Jumping_Pad_2.png")
         jump_pad_img3 = pygame.image.load("assets/Jumping_Pad_3_Stage_1.png")
+        exit_img = pygame.image.load("Exit.png")
 
 
 
@@ -39,6 +40,13 @@ class World():
                     self.tile_list.append(tile)
                 if tile == 4:
                     img = pygame.transform.scale(jump_pad_img3, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 5:
+                    img = pygame.transform.scale(exit_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
                     img_rect.x = col_count * tile_size
                     img_rect.y = row_count * tile_size
