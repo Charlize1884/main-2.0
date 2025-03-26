ccdd596cd55cbb88849054b1ff20dd200e498344
Charlize1884 <cbololoi@students.uni-mainz.de>
2025-03-26 15:40:24 +0100

    adding

--- EiP-Praktikum/Patroling_Ghost.py
+++ EiP-Praktikum/Patroling_Ghost.py
@@ -5,5 +5,5 @@ class Patroling_Ghost(Patroling_Enemy):
     def __init__(self, x_coord, y_coord, tile_size):
         Patroling_Enemy.__init__(self, x_coord, y_coord,'assets/Ghost_1.png', 2, 1, 14/6, 0, tile_size, 1)
     def update(self):
-            self.patrol(100)
+            self.patrol(50)
 
