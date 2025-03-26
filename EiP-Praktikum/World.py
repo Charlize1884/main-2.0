37628e57042a6fb9afbf40c86363aaa2a67a426a
qphu1905 <quobui@students.uni-mainz.de>
2025-03-26 14:41:33 +0100

    Clean up World.py, map load using the load_map method instead of __init__.

--- EiP-Praktikum/World.py
+++ EiP-Praktikum/World.py
@@ -2,13 +2,14 @@ import pygame
 from World_building_blocks import *
 from Patroling_Ghost import Patroling_Ghost
 
-class World():
-    def __init__(self,data, tile_size):
+class World:
+    def __init__(self):
         self.tile_list = []
         self.fake_tile_list = []
         self.enemy_list = []
 
 
+    def load_map(self, data, tile_size):
         row_count = 0
         for row in data:
             col_count = 0
@@ -37,17 +38,20 @@ class World():
                 elif tile == "g":
                     object = Patroling_Ghost(col_count*tile_size, row_count * tile_size, tile_size)
                     self.enemy_list.append(object)
-
                 col_count += 1
             row_count += 1
+
 
     def draw(self, screen, camera_offset_x, camera_offset_y):
         for tile in self.tile_list:
             offset_rect = pygame.Rect(tile.rect.x + camera_offset_x, tile.rect.y+camera_offset_y, tile.rect.width, tile.rect.height)
             screen.blit(tile.image, offset_rect)
+
         for tile in self.fake_tile_list:
             offset_rect = pygame.Rect(tile.rect.x + camera_offset_x, tile.rect.y+camera_offset_y, tile.rect.width, tile.rect.height)
             screen.blit(tile.image, offset_rect)
+
+
     def draw_enemies(self, screen, camera_offset_x, camera_offset_y):
         for enemy in self.enemy_list:
             offset_rect = pygame.Rect(enemy.rect.x + camera_offset_x, enemy.rect.y+camera_offset_y, enemy.rect.width, enemy.rect.height)
